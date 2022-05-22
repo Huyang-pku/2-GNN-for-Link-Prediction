06f970dca4fda03e922f2213e800cd2c602f5dff
little_buster <1800011848@pku.edu.cn>
2022-05-22 17:36:25 +0800

    kg_init

--- work.py
+++ work.py
@@ -1,58 +1,12 @@
 import subprocess
 import time
-
-dir = "opt"
-
-
-def work(dg):
-    dataset, gpu_id = dg
-    cmd = f"nohup python main.py --device {gpu_id} --dataset {dataset} > {dir}/{dataset}_{gpu_id} 2>&1 &"
-    print(cmd, flush=True)
-    subprocess.call(cmd, shell=True)
-
-
-def test(dg):
-    dataset, gpu_id = dg
-    cmd = f"nohup python main.py --test --device {gpu_id} --dataset {dataset} > {dir}/{dataset}_{gpu_id}.test 2>&1 &"
-    print(cmd, flush=True)
-    subprocess.call(cmd, shell=True)
-
-dir = "reproduceHY"
-def reproduce(dg):
-    dataset, gpu_id = dg
-    cmd = f"nohup python main.py --reproduce --device {gpu_id} --dataset {dataset} > {dir}/{dataset}_{gpu_id} 2>&1 &"
-    print(cmd, flush=True)
-    subprocess.call(cmd, shell=True)
-
-
-def wait():
-    while True:
-        ret = subprocess.check_output("nvidia-smi -q -d Memory | grep  Used",
-                                      shell=True)
-        sel = [
-            _ for _ in ret.split()
-            if b"U" not in _ and b":" not in _ and b"M" not in _
-        ]
-        load = [int(i) for i in sel]
-        for i in range(len(load) // 2):
-            if sum(load[2 * i:2 * i + 2]) < 50:
-                return i
-        time.sleep(5)
-
-
-for i, ds in enumerate([
-        "Power"
-]):
-    dev = wait()
-    reproduce((ds, dev))
-    time.sleep(10)
-
+datasets = ["GraIL-BM_fb237_v1", "GraIL-BM_fb237_v2", "GraIL-BM_fb237_v3", "GraIL-BM_fb237_v4", 
+    "GraIL-BM_nell_v1", "GraIL-BM_nell_v2", "GraIL-BM_nell_v3", "GraIL-BM_nell_v4",
+    "GraIL-BM_WN18RR_v1", "GraIL-BM_WN18RR_v2", "GraIL-BM_WN18RR_v3", "GraIL-BM_WN18RR_v4"]
 '''
-for i, ds in enumerate([
-        "Celegans", "NS", "Power", "Router", "Ecoli", "PB", "USAir", "Yeast",
-        "Wikipedia", "Wikipedia", "arxiv"
-]):
-    dev = wait()
-    reproduce((ds, dev))
-    time.sleep(10)
+for ds in datasets:
+    subprocess.call(f"python negative_sampling.py --dataset {ds} --sample_rate 3 &", shell=True)
 '''
+
+for i, ds in enumerate(datasets[:3]):
+    subprocess.call(f"CUDA_VISIBLE_DEVICES={1} nohup python train.py --dataset {ds} >> out/{ds}_{i} 2>&1 &", shell=True)
